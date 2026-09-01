c11f6b1d0fc8a0ab9823e340559f206f29a9f5d0
ShagunGupta56 <shagunguptanbj2005@gmail.com>
2026-09-01 20:37:46 +0530

    Add production admin setup

--- Server/changeAdminPassword.js
+++ Server/changeAdminPassword.js
@@ -3,37 +3,64 @@ require("dotenv").config();
 const bcrypt = require("bcryptjs");
 const db = require("./config/db");
 
-const newPassword = process.env.NEW_ADMIN_PASSWORD;
+const newPassword =
+    process.env.NEW_ADMIN_PASSWORD;
 
 if (!newPassword) {
-    console.log("NEW_ADMIN_PASSWORD is missing.");
+
+    console.log(
+        "NEW_ADMIN_PASSWORD is missing."
+    );
+
     process.exit(1);
 }
 
-bcrypt.hash(newPassword, 12, (err, hash) => {
 
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-
-    const sql = `
-        UPDATE admins
-        SET password = ?
-        WHERE username = 'admin'
-    `;
-
-    db.query(sql, [hash], (err, result) => {
+bcrypt.hash(
+    newPassword,
+    12,
+    (err, hash) => {
 
         if (err) {
+
             console.log(err);
+
             process.exit(1);
         }
 
-        console.log(
-            "Admin password changed successfully."
-        );
 
-        db.end();
-    });
-});
+        const sql = `
+            INSERT INTO admins (
+                username,
+                password
+            )
+            VALUES ('admin', ?)
+
+            ON DUPLICATE KEY UPDATE
+                password = VALUES(password)
+        `;
+
+
+        db.query(
+            sql,
+            [hash],
+            (err) => {
+
+                if (err) {
+
+                    console.log(err);
+
+                    process.exit(1);
+                }
+
+
+                console.log(
+                    "Admin created / password updated successfully."
+                );
+
+
+                db.end();
+            }
+        );
+    }
+);
