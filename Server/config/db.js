a81f8df6d184310990a4f77464b6ba7557889fbe
ShagunGupta56 <shagunguptanbj2005@gmail.com>
2026-09-02 07:39:52 +0530

    Use MySQL connection pool for production

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,20 +1,33 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
+
     if (err) {
-        console.log("Database connection failed:", err);
+        console.log(
+            "Database connection failed:",
+            err
+        );
+
         return;
     }
 
-    console.log("MySQL connected successfully!");
+    console.log(
+        "MySQL connection pool created successfully!"
+    );
+
+    connection.release();
 });
 
-module.exports = connection;
+module.exports = pool;
